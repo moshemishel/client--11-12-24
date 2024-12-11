fe0c7f0b3b46ae917432f6ae988f5cd4908241d5
Moshe Mishel <moshmish@h-MacBook-Air-sl-Moshe.local>
2024-12-11 13:02:17 +0200

    v1

--- components/tabsByRole.ts
+++ components/tabsByRole.ts
@@ -1,11 +1,11 @@
 import { User, TabsByRole } from '@/types/context/AuthContext';
-import pagesByRole from '@/context/pagesByRole';
+import pagesByRole from '@/context/pagesByRoll';
 
 const getTabsByRole = (user: User | null): TabsByRole=> {
     if (user === null){
         return []
     }
-    return pagesByRole(user.role ,user.userName)
+    return pagesByRole(user.role! ,user.userName!)
 }
 
 export default getTabsByRole;
---
+++ context/pagesByRoll.ts
@@ -0,0 +1,32 @@
+import {PagesByRole } from '@/types/context/AuthContext';
+
+
+
+// const getPagesByRole = (user: User | null): PagesByRole=> {
+const getPagesByRole = (role: number, userName :string): PagesByRole=> {
+
+    switch (role) {
+        case 0: // Super admin of a app
+            return [
+                { name: "paymentForm", link: `/superAdmin/${userName}/paymentForm` },
+                { name: "userCenter", link: `/superAdmin/${userName}/userCenter` },
+                { name: "Dashboard", link: `/superAdmin/${userName}/CustomersTransactions` },
+                { name: "usersDashboard", link: `/superAdmin/${userName}/usersDashboard` },
+            ];
+        case 10: // admin of account
+        case 20: // co-owner of account
+        case 50: // Store manager
+        case 100: // Accountant of store
+            return [
+                { name: "paymentForm", link: `/${userName}/paymentForm` },
+                { name: "userCenter", link: `/${userName}/userCenter` },
+                { name: "Dashboard", link: `/${userName}/dashboard` },
+                { name: "usersDashboard", link: `/${userName}/usersDashboard` },
+            ];            
+        default:
+            return []; // משתמש ללא גישה
+    }
+}; 
+export default getPagesByRole;
+
+
